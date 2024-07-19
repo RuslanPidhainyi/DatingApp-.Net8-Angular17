f9e993d10772187400b72bb0c17e7e23e67d0357
Ruslan <136593314+RuslanPidhainyi@users.noreply.github.com>
2024-07-19 18:11:46 +0200

    Added user selection on the registration page - v 4.0.6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 //import { CommonModule, NgFor } from '@angular/common';//W Bibliotece Angulara 17 CommonModule - jest część NgFor. NgFor - iteruję nam liste (czyli cos podobnego jak foreach w C#). Jezeli bedziemy wykorzystac sam NgFor, to sensownie zostawic go, a nie CommonModule.
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
@@ -17,15 +16,10 @@ import { HomeComponent } from "./home/home.component";
 //implements OnInit - Oczekuje od nas utworzenia func o nazwie ng on init, w ktorej moze wykonywac kod wymagania w tym konkretnym zdarzeniu cykl zycia 
 export class AppComponent implements OnInit {
   //Kiedy chcemy uzyskac dostep do wlasciwosci class, musimy uzyc tego (czylic block kodu func ngOnInit):
-  http = inject(HttpClient);
-  private accountService  = inject(AccountService)
-  title = 'DatingApp';
-  users: any;
-
+  private accountService  = inject(AccountService);
 
   //ng on Init - wykonuje nasze wywołanie HTTP
   ngOnInit(): void {
-   this.getUsers();
    this.setCurrentUser();
   }
 
@@ -37,38 +31,5 @@ export class AppComponent implements OnInit {
     this.accountService.currentUser.set(user);
   }
 
-  getUsers(){
-     /*
-      Co robi nasz Kod, a dokladnie metoda get?
-      
-      Konstruuje więc żądanie Get, które interpretuje treść jako JSON i zwraca treść odpowiedzi jako obiekt parsowany z JSON
-
-      A jesli spojrzymy na zwrot, to zwraca on coś, co nazywa się obserwowalnym adresem URL treści odpowiedzi jako objekt JSON - Teraz Angular intensywnie korzysta z tych rzeczy zwanych OBSERVABLES.
-
-      pomysl o OBSERVABLES jako o strumieniu danych i mozemy obserwowac ten strumien, w tym przypadku obserwujemy odpowiedz HTTP powracającą z naszego serwera API 
-
-      Typowym sposobem radzenia sobie z kodem asynchronicznym TS jest uzycie Promise lub asynchroniznych await, podobnie jak w C# Ale Angular jest inny. Wykorzystuje on obserwowalny.
-
-      Jak wchodzimy w interakcje z obserwowalnym a 1 Zasada obserwowalnych, jesli chcesz z tym ządaniem HTTP jest takie, ze nic się nie stanie, jesli zostawiamy nasz kod w ten sposob, z tym żądaniem HTTP jest takie, że nic się nie stanie, jesli zostawimy nasz kod w ten sposób. Nie wykonujemy żądania HTTP.
-    */
-
-    // W ten sposob uzyskujemy dostęp do wlasciwosci klasy
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      //func
-      next: response => this.users = response,
-
-      //Co biedzie jezeli wystąpi błąd
-      error: error => console.log(error),
-
-      //Co ma się wydarzyć po skonczeniu działania obserwowalnego - i znow jest to kolejna func, ktorą mozemy dodać, aby zrobić coś gdy coś się zakonczy
-      complete: () => console.log('Request has completed')
-
-    }) //.http.get - poniewarz zamierzymy wykonac żądanie get. Następnie okreslamy adress URL, skąd bedziemy pobierac dane - umorzliwia urzytkownikam pobieranie dane
-
-    /*
-      Obserwowalne są domyslnie leniwy i jesli nikt nie subskrebuje obserwalnego, nie robi on nic.
-
-      Więc jesli chcemy, aby to się wykonało, musimy najpierw zasubskrybowac obserwowalne a potem mozemy powieszic co się stanie po subskrypcji
-    */
-  }
+  
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  // pierwszy sposob //
+  //@Input() usersFromHomeComponent: any; //za pomocy dekorotywnego "input'u" i wlasciwosci "input'a", chcemy przekazac wlasciwosci ("users" w pliku "register.component.html") do komponentu podrzednego
+
+  // drugi sposob - dziala z wersji 17.3^ //
+  usersFromHomeComponent= input.required<any>()
   model: any = {}
 
   register() {

a9082121507ab3bbbafdf5756d9cc0325b7b33f7
Ruslan <136593314+RuslanPidhainyi@users.noreply.github.com>
2024-08-10 19:16:11 +0200

    Added a toast service for notifications - v 4.0.12

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 //import { NgIf } from '@angular/common'; /* Директива *ngIf зазвичай використовується в Angular-шаблонах для контролю видимості елементів. Якщо вираз всередині *ngIf оцінюється як true, елемент та його діти будуть включені в DOM; якщо оцінюється як false, елемент та його діти будуть видалені з DOM.              ALE derektywa angulara "NgIf" od wersji 17 angular nieuzywamy */
 
 @Component({
@@ -14,19 +15,22 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toaster = inject(ToastrService)
   model: any = {};
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: _ => {
+        this.router.navigateByUrl('/members')
       },
-      error: error => console.log(error),
+      error: error => this.toaster.error(error.error),
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 
 }

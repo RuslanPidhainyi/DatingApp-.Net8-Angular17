771edfaeeaff003a067743f12532781aa586b96a
Ruslan <136593314+RuslanPidhainyi@users.noreply.github.com>
2024-08-11 16:55:46 +0200

    Added a new theme - v 4.0.15

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,12 +4,13 @@ import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 //import { NgIf } from '@angular/common'; /* Директива *ngIf зазвичай використовується в Angular-шаблонах для контролю видимості елементів. Якщо вираз всередині *ngIf оцінюється як true, елемент та його діти будуть включені в DOM; якщо оцінюється як false, елемент та його діти будуть видалені з DOM.              ALE derektywa angulara "NgIf" od wersji 17 angular nieuzywamy */
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive], //importujemy tutaj rozne dodatke do naszego projektu (np.FormsModule lub derektywe angulara "NgIf" )
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe], //importujemy tutaj rozne dodatke do naszego projektu (np.FormsModule lub derektywe angulara "NgIf" )
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })

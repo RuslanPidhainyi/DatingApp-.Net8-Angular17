a05459707fe1fe28ea67653847057288e129c843
Ruslan <136593314+RuslanPidhainyi@users.noreply.github.com>
2024-09-21 15:40:36 +0200

    Adding a reusable date input - v 10.0.3

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,20 +1,39 @@
-import { Component, EventEmitter, inject, input, Input, OnInit, output, Output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  input,
+  Input,
+  OnInit,
+  output,
+  Output,
+} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { JsonPipe, NgIf } from '@angular/common';
-import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { TextInputComponent } from '../_forms/text-input/text-input.component';
+import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
-
   private accountService = inject(AccountService);
+  private fb = inject(FormBuilder);
   private toaster = inject(ToastrService);
 
   // pierwszy sposob //
@@ -26,34 +45,51 @@ export class RegisterComponent implements OnInit {
   /******************************************************************************************************** */
 
   // pierwszy sposob //
-  //@Output() cancelRegister = new EventEmitter() //dekorator "@output()" czyli dekorator wyjsciowy - ktory bedzie cos robic i zaincjąwaliscmy EventEmitter - emiter zdarzen 
+  //@Output() cancelRegister = new EventEmitter() //dekorator "@output()" czyli dekorator wyjsciowy - ktory bedzie cos robic i zaincjąwaliscmy EventEmitter - emiter zdarzen
 
   // drugi sposob - dziala z wersji 17.3^ //
   cancelRegister = output<boolean>();
 
-  model: any = {}
+  model: any = {};
   registerForm: FormGroup = new FormGroup({});
+  maxDate = new Date();
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   initializeForm() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]), 
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]],
+      confirmPassword: ['', [
+        Validators.required,
+        this.matchValues('password'),
+      ]],
     });
 
     this.registerForm.controls['password'].valueChanges.subscribe({
-      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
-    })
+      next: () =>
+        this.registerForm.controls['confirmPassword'].updateValueAndValidity(),
+    });
   }
 
   matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
-    }
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { isMatching: true };
+    };
   }
 
   register() {
@@ -68,6 +104,6 @@ export class RegisterComponent implements OnInit {
   }
 
   cancel() {
-    this.cancelRegister.emit(false); //kiedy klikamy na przycisk wewnetrz naszego komponentu podrzedniego emitujemy wartosc false 
+    this.cancelRegister.emit(false); //kiedy klikamy na przycisk wewnetrz naszego komponentu podrzedniego emitujemy wartosc false
   }
 }

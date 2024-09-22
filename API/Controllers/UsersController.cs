e118712c55ac0ba8920cea3af62aa9a406ec73bc
Ruslan <136593314+RuslanPidhainyi@users.noreply.github.com>
2024-09-22 22:20:52 +0200

    Using the angular bootstrap pagination component

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -16,14 +17,16 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
 {
    //Metoda do zwracania odpowiedzi HTTP  do klienta 
    [HttpGet] //Ządania HTTP Get 
-   public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() /*publiczna metoda.
+   public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams) /*publiczna metoda.
 
                                                          Result action - jako typ rzeczy, ktore zamierzamy zwrocic z tego  punktu koncowego API
 
                                                          Następnie okreslamy typ  zwracanej rzeczy  - teraz zwrocimy liste użytkownikow (Istneje  wiele róznych  rodzajów list w C#, jedna z metod, ktorej mozemy do tego użyc jest enumarable - uzywane tylko dla kolekcji okreslonego typu) - zwrocimy IEnumereble typu appUser*/
    {
       //Stąd możemy zwracac odpowiedz HTTP
-      var users = await userRepository.GetMembersAsync(); //w ten sposob otrzymamy liste user'ow z naszej bazy
+      var users = await userRepository.GetMembersAsync(userParams); //w ten sposob otrzymamy liste user'ow z naszej bazy
+
+      Response.AddPaginationHeader(users); 
 
       return Ok(users); //Ok - nie dba o to co umiescili. Bo to co umiscili nie jest az tak dobre, przez to ze uzywamy kolekcje "IEnumerable"
    }

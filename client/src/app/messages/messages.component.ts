fd5258ccfcca6a46c50debb8fa27abbe78da1ce5
Ruslan <ruslanpidhainyi02@gmail.com>
2024-09-27 18:44:03 +0200

    Designing the inbox - v 13.0.6

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessageService } from '../_services/message.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { Message } from '../_models/message';
+import { RouterLink } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterLink, PaginationModule],
   templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css'
+  styleUrl: './messages.component.css',
 })
 export class MessagesComponent implements OnInit {
   messageService = inject(MessageService);
@@ -14,17 +20,26 @@ export class MessagesComponent implements OnInit {
   pageNumber = 1;
   pageSize = 5;
 
-
   ngOnInit(): void {
     this.loadMessages();
   }
 
   loadMessages() {
-    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container)
+    this.messageService.getMessages(
+      this.pageNumber,
+      this.pageSize,
+      this.container
+    );
   }
 
-  pageChanged(event: any){
-    if(this.pageNumber !== event.page) {
+  getRoute(message: Message) {
+    if (this.container === 'Outbox')
+      return `/members/${message.recipientUsername}`;
+    else return `/members/${message.senderUsername}`;
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
       this.pageNumber = event.page;
       this.loadMessages();
     }

688ab2b080e6818e4eadf502ddb3217ab7d37fab
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-02 23:43:53 +0200

    Optimizing the messages - v 15.0.12

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -42,12 +42,11 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
     public async Task<IEnumerable<MessageDto>> GetMessagesThread(string currentUsername, string recipientUsername)
     {
         var messages = await context.Messages
-            .Include(x => x.Sender).ThenInclude(x => x.Photos)
-            .Include(x => x.Recipient).ThenInclude(x => x.Photos)
             .Where(x => x.RecipientUsername == currentUsername && x.RecipientDeleted == false && x.SenderUsername  == recipientUsername || 
                         x.SenderUsername == currentUsername && x.SenderDeleted == false && x.RecipientUsername == recipientUsername
             )
             .OrderBy(x => x.MessageSent)
+            .ProjectTo<MessageDto>(mapper.ConfigurationProvider)
             .ToListAsync();
 
         var unreadMessages = messages.Where(x => x.DateRead == null && x.RecipientUsername == currentUsername).ToList();
@@ -58,7 +57,7 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
             await context.SaveChangesAsync();
         }
 
-        return mapper.Map<IEnumerable<MessageDto>>(messages);
+        return messages;
     }
 
     public async Task<Message?> GetMessage(int id)
@@ -91,5 +90,13 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
         return await context.Groups
             .Include(x => x.Connections)
             .FirstOrDefaultAsync(x => x.Name == groupName);
+    }
+
+    public async Task<Group?> GetGroupForConnection(string connectionId)
+    {
+        return await context.Groups
+        .Include(x => x.Connections)
+        .Where(x => x.Connections.Any(c => c.ConnectionId == connectionId))
+        .FirstOrDefaultAsync();
     }
 }
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -9,6 +9,8 @@ import {
 } from './paginationHelpers';
 import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
 import { User } from '../_models/user';
+import { group } from '@angular/animations';
+import { Group } from '../_models/group';
 
 @Injectable({
   providedIn: 'root',
@@ -37,6 +39,19 @@ export class MessageService {
 
     this.hubConnection.on('NewMessage', message => {
       this.messageThread.update(messages => [...messages, message])
+    });
+
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if(group.connections.some(x => x.username === otherUsername)) {
+        this.messageThread.update(messages => {
+          messages.forEach(message => {
+            if(!message.dateRead) {
+              message.dateRead = new Date(Date.now());
+            }
+          })
+          return messages;
+        })
+      }
     })
   }
 
